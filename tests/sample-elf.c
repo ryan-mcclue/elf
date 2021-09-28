01646c84b474501b4b4155deada9b538faacfbf1
Ryan McClue <re.mcclue@protonmail.com>
2021-09-28 18:10:41 +1000

    Add basic shared object file for testing

---
+++ tests/sample-elf.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: zlib-acknowledgement
+
+#include <printf.h>
+
+void
+asterix_name(char buf[], char *name)
+{
+  sprintf(buf, "***** %s *****", name);
+}
+
+int
+add_int(int x, int y)
+{
+  return x + y;
+}
+
+int
+sub_int(int x, int y)
+{
+  return x - y;
+}
+
