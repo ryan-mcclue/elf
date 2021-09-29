21e44d67c609c1f3a14800802cb992f5547611cf
Ryan McClue <re.mcclue@protonmail.com>
2021-09-29 16:35:34 +1000

    Add Ctest usage

--- tests/static-elf-tests.c
+++ tests/static-elf-tests.c
@@ -11,7 +11,7 @@
 void
 test(void **state)
 {
-  ElfLoadResult elf_load_result = elf_load("../tests/sample-lib.so");
+  ElfLoadResult elf_load_result = elf_load("sample-lib.so");
 
   int (*add_int)(int, int) = elf_lookup_function(&elf_load_result, "add_int");
   assert_int_equal(add_int(10, 20), 30);
